a84b618b97fbc3dedcea7ab6ccf84eb147625210
khaledvic <khaledvic@gmail.com>
2014-07-08 18:20:13 +0300

    finalizing try node

--- compiler/CatchNode.h
+++ compiler/CatchNode.h
@@ -10,10 +10,15 @@ public:
 	Type* exp;
 	string idn;
 	static int lbl_count ;
-	int numId=0;
+	int numId = 0;
+	int try_id = 0;
 	CatchNode(ScoopNode* scoop, Type* type, string identifier) :VoidNode(scoop), exp(type), idn(identifier)
 	{
+		scoop->add_variable(new Variable(identifier,type,0));
 		//add to scoops
+	}
+	virtual void generateCode(){
+		MIPS_ASM::label(getCatchLabel());
 	}
 	string getCatchLabel(){
 		return "catch_" + std::to_string(getNumId());
