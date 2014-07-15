d65a23dce2b777bb9c52df78348b5cc48b0e3017
khaledvic <khaledvic@gmail.com>
2014-07-15 19:35:49 +0300

    added cast node

--- compiler/ast/CastNode.h
+++ compiler/ast/CastNode.h
@@ -14,7 +14,30 @@ public:
 	CastNode(ScoopNode* scoop,Type* toType,Node* node):Node(scoop),_toType(toType),_node(node)
 	{
 	}
-	
+	virtual void generateCode()
+	{
+		if (getType()== symbolTable->getType("error_type"))
+			return;
+		Type* intType = symbolTable->getType("int");
+		Type* boolType = symbolTable->getType("bool");
+		Type* floatType = symbolTable->getType("float");
+		auto fromt = _node->getType();
+		if (_toType == intType && fromt==floatType)
+		{
+			MIPS_ASM::popf("f1");
+			MIPS_ASM::add_instruction("cvt.w.s $f1,$f1\n");
+			MIPS_ASM::pushf("f1");
+
+		}
+		if (_toType == floatType && fromt == intType)
+		{
+			MIPS_ASM::popf("f1");
+			MIPS_ASM::add_instruction("cvt.s.w $f1,$f1\n");
+			MIPS_ASM::pushf("f1");
+
+		}
+
+	}
 	virtual Type* generateType()
 	{
 		if (_node->getType()==NULL)
